9fcebe7354294ecf5b85b062fdffad11a42067e8
Muhammad wasi <wasiarain819@gmail.com>
2021-09-05 18:28:49 +0500

    Fix: SSR-MaterialUI-Styles

--- components/Layout.js
+++ components/Layout.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import Head from 'next/head';
 import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
+import useStyles from '../utils/styles';
 
 export default function Layout({ children }) {
+  const classes = useStyles();
   return (
     <div>
       <Head>
         <title>Next Amazon</title>
       </Head>
 
-      {/*Navbar section  */}
-      <AppBar position="static">
+      {/*  Navbar section  */}
+      <AppBar position="static" className={classes.navbar}>
         <Toolbar>
           <Typography>Amazon</Typography>
         </Toolbar>
       </AppBar>
       {/* Main Section */}
-      <Container>{children}</Container>
+      <Container className={classes.main}>{children}</Container>
 
       {/* Footer */}
-      <footer>
+      <footer className={classes.footer}>
         <Typography>&copy; All right reserved. Next Amazon.</Typography>
       </footer>
     </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import { useEffect } from 'react';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  //TODO: Fix issue of server side rendering of material-ui elements
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
